28996f39eb84c2f7209077bbc0b25a67a92c135d
jdwoo88 <jdwoo.88@yahoo.com>
2021-03-19 16:17:44 +0800

    Initial Commit
    Initial Commit - Complete Chapter 3

---
+++ tut-vue3/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    {{user.userName}} - {{fullName}} 
+    <strong>Followers: {{followers}}</strong>
+    <button @click="followUser">Follow</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        userName: 'jwoo',
+        firstName: 'John',
+        lastName: 'Douglas',
+        email: 'john@douglas.com',
+        isAdmin: true
+      }
+    }
+  },
+  watch:{
+    followers(newFollowersCount, oldFollowersCount){
+      if (oldFollowersCount < newFollowersCount){
+        console.log(`${this.user.userName} has gained a follower.` )
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser(){
+      this.followers++
+    }
+  },
+  created() {
+    console.log('created');
+  },
+  mounted(){
+    this.followUser();
+    console.log('mounted');
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
